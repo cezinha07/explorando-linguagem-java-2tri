52570bbf19380335d2dc4ac1399bbfcf020682e1
cezinha07 <130381085+cezinha07@users.noreply.github.com>
2023-12-10 04:59:52 -0300

    Add files via upload

---
+++ java/2-TrablhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("trabalhando com variaveis");
+console.log("trabalhando com variaveis");
+// JS é case sensitive
+
+const idade = 16;
+const nome = "Ronaldo";
+
+//nao fazer
+//mes = "janeiro";
